942ef5a7ca481e56310b470b1117ebd8990b2397
kitty <914384274@qq.com>
2026-01-29 12:31:18 +0800

    add opencv rb

---
+++ opencv.rb
@@ -0,0 +1,218 @@
+class Opencv < Formula
+  desc "Open source computer vision library"
+  homepage "https://opencv.org/"
+  license "Apache-2.0"
+  revision 9
+
+  stable do
+    url "https://github.com/opencv/opencv/archive/refs/tags/4.12.0.tar.gz"
+    sha256 "44c106d5bb47efec04e531fd93008b3fcd1d27138985c5baf4eafac0e1ec9e9d"
+
+    resource "contrib" do
+      url "https://github.com/opencv/opencv_contrib/archive/refs/tags/4.12.0.tar.gz"
+      sha256 "4197722b4c5ed42b476d42e29beb29a52b6b25c34ec7b4d589c3ae5145fee98e"
+
+      livecheck do
+        formula :parent
+      end
+    end
+
+    # Backport support for FFmpeg 8.0
+    patch do
+      url "https://github.com/opencv/opencv/commit/90c444abd387ffa70b2e72a34922903a2f0f4f5a.patch?full_index=1"
+      sha256 "5b662eea7b5de1dac3e06895c711955c9d1515d1202191b68594f4f9cfa23242"
+    end
+  end
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  no_autobump! because: :requires_manual_review
+
+  bottle do
+    sha256 arm64_sonoma:  "cf8d7a86ea020d5824c4ef0253aee998edd973d81e4ed108c3f5c24acd95b426"
+    sha256 arm64_ventura: "f31fa216051e53763579dca12931ed4bbb30e243dc7b07c6ed30e6e8148e7f89"
+    sha256 sonoma:        "36b492895571ab5db9da0ffd2fe2896a5c163653693b1aa8801c912cfd4b671d"
+    sha256 ventura:       "8b5d14e644fd2b86251bd455a1fee89b46bc7e9321e75200b808f29fec8b00fd"
+    sha256 x86_64_linux:  "86093877c802805a7b8d98d8c960b34cb1eda1e62d4ed33737bd88d4f6b86fc4"
+  end
+
+  head do
+    url "https://github.com/opencv/opencv.git", branch: "master"
+
+    resource "contrib" do
+      url "https://github.com/opencv/opencv_contrib.git", branch: "master"
+    end
+  end
+
+  depends_on "cmake" => :build
+  depends_on "pkgconf" => :build
+  depends_on "python-setuptools" => :build
+  depends_on "abseil"
+  depends_on "ceres-solver"
+  depends_on "eigen"
+  depends_on "ffmpeg"
+  depends_on "freetype"
+  depends_on "gflags"
+  depends_on "glog"
+  depends_on "harfbuzz"
+  depends_on "jpeg-turbo"
+  depends_on "jsoncpp"
+  depends_on "libpng"
+  depends_on "libtiff"
+  depends_on "numpy"
+  depends_on "openblas"
+  depends_on "openexr"
+  depends_on "openjpeg"
+  depends_on "openvino"
+  depends_on "protobuf"
+  depends_on "python@3.13"
+  depends_on "tbb"
+  depends_on "tesseract"
+  depends_on "vtk"
+  depends_on "webp"
+
+  uses_from_macos "zlib"
+
+  on_macos do
+    depends_on "glew"
+    depends_on "imath"
+    depends_on "libarchive"
+  end
+
+  on_linux do
+    depends_on "cairo"
+    depends_on "gdk-pixbuf"
+    depends_on "glib"
+    depends_on "gtk+3"
+  end
+
+  def python3
+    "python3.13"
+  end
+
+  def install
+    resource("contrib").stage buildpath/"opencv_contrib"
+
+    # Avoid Accelerate.framework
+    ENV["OpenBLAS_HOME"] = Formula["openblas"].opt_prefix
+
+    # Remove bundled libraries to make sure formula dependencies are used
+    libdirs = %w[ffmpeg libjasper libjpeg libjpeg-turbo libpng libtiff libwebp openexr openjpeg protobuf tbb zlib]
+    libdirs.each { |l| rm_r(buildpath/"3rdparty"/l) }
+
+    args = %W[
+      -DCMAKE_CXX_STANDARD=17
+      -DCMAKE_OSX_DEPLOYMENT_TARGET=
+      -DBUILD_JASPER=OFF
+      -DBUILD_JPEG=OFF
+      -DBUILD_OPENEXR=OFF
+      -DBUILD_OPENJPEG=OFF
+      -DBUILD_PERF_TESTS=OFF
+      -DBUILD_PNG=OFF
+      -DBUILD_PROTOBUF=OFF
+      -DBUILD_TBB=OFF
+      -DBUILD_TESTS=OFF
+      -DBUILD_TIFF=OFF
+      -DBUILD_WEBP=OFF
+      -DBUILD_ZLIB=OFF
+      -DBUILD_opencv_hdf=OFF
+      -DBUILD_opencv_java=OFF
+      -DBUILD_opencv_text=ON
+      -DOPENCV_ENABLE_NONFREE=ON
+      -DOPENCV_EXTRA_MODULES_PATH=#{buildpath}/opencv_contrib/modules
+      -DOPENCV_GENERATE_PKGCONFIG=ON
+      -DPROTOBUF_UPDATE_FILES=ON
+      -DWITH_1394=OFF
+      -DWITH_CUDA=OFF
+      -DWITH_EIGEN=ON
+      -DWITH_FFMPEG=ON
+      -DWITH_GPHOTO2=OFF
+      -DWITH_GSTREAMER=OFF
+      -DWITH_JASPER=OFF
+      -DWITH_OPENEXR=ON
+      -DWITH_OPENGL=OFF
+      -DWITH_OPENVINO=ON
+      -DWITH_QT=OFF
+      -DWITH_TBB=ON
+      -DWITH_VTK=ON
+      -DBUILD_opencv_python2=OFF
+      -DBUILD_opencv_python3=ON
+      -DPYTHON3_EXECUTABLE=#{which(python3)}
+    ]
+
+    args += if OS.mac?
+      # Requires closed-source, pre-built Orbbec SDK on macOS
+      ["-DWITH_OBSENSOR=OFF"]
+    else
+      # Disable precompiled headers and force opencv to use brewed libraries on Linux
+      %W[
+        -DENABLE_PRECOMPILED_HEADERS=OFF
+        -DJPEG_LIBRARY=#{Formula["jpeg-turbo"].opt_lib}/libjpeg.so
+        -DOpenBLAS_LIB=#{Formula["openblas"].opt_lib}/libopenblas.so
+        -DOPENEXR_ILMIMF_LIBRARY=#{Formula["openexr"].opt_lib}/libIlmImf.so
+        -DOPENEXR_ILMTHREAD_LIBRARY=#{Formula["openexr"].opt_lib}/libIlmThread.so
+        -DPNG_LIBRARY=#{Formula["libpng"].opt_lib}/libpng.so
+        -DPROTOBUF_LIBRARY=#{Formula["protobuf"].opt_lib}/libprotobuf.so
+        -DPROTOBUF_INCLUDE_DIR=#{Formula["protobuf"].include}
+        -DPROTOBUF_PROTOC_EXECUTABLE=#{Formula["protobuf"].bin}/protoc
+        -DTIFF_LIBRARY=#{Formula["libtiff"].opt_lib}/libtiff.so
+        -DWITH_V4L=OFF
+        -DZLIB_LIBRARY=#{Formula["zlib"].opt_lib}/libz.so
+      ]
+    end
+
+    # Ref: https://github.com/opencv/opencv/wiki/CPU-optimizations-build-options
+    ENV.runtime_cpu_detection
+    if Hardware::CPU.intel? && build.bottle?
+      cpu_baseline = if OS.mac? && MacOS.version.requires_sse42?
+        "SSE4_2"
+      else
+        "SSSE3"
+      end
+      args += %W[-DCPU_BASELINE=#{cpu_baseline} -DCPU_BASELINE_REQUIRE=#{cpu_baseline}]
+    end
+
+    system "cmake", "-S", ".", "-B", "build_shared", *args, *std_cmake_args
+    inreplace "build_shared/modules/core/version_string.inc", "#{Superenv.shims_path}/", ""
+    system "cmake", "--build", "build_shared"
+    system "cmake", "--install", "build_shared"
+
+    system "cmake", "-S", ".", "-B", "build_static", *args, *std_cmake_args, "-DBUILD_SHARED_LIBS=OFF"
+    inreplace "build_static/modules/core/version_string.inc", "#{Superenv.shims_path}/", ""
+    system "cmake", "--build", "build_static"
+    lib.install buildpath.glob("build_static/{lib,3rdparty/**}/*.a")
+
+    # Prevent dependents from using fragile Cellar paths
+    inreplace lib/"pkgconfig/opencv#{version.major}.pc", prefix, opt_prefix
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~CPP
+      #include <opencv2/core.hpp>
+      #include <opencv2/imgcodecs.hpp>
+      #include <opencv2/opencv.hpp>
+      #include <iostream>
+      int main() {
+        std::cout << CV_VERSION << std::endl;
+        cv::Mat img = cv::imread("#{test_fixtures("test.jpg")}", cv::IMREAD_COLOR);
+        if (img.empty()) {
+          std::cerr << "Could not read test.jpg fixture" << std::endl;
+          return 1;
+        }
+        return 0;
+      }
+    CPP
+    system ENV.cxx, "-std=c++17", "test.cpp", "-I#{include}/opencv4", "-o", "test",
+                    "-L#{lib}", "-lopencv_core", "-lopencv_imgcodecs"
+    assert_equal version.to_s, shell_output("./test").strip
+
+    # The test below seems to time out on Intel macOS.
+    return if OS.mac? && Hardware::CPU.intel?
+
+    output = shell_output("#{python3} -c 'import cv2; print(cv2.__version__)'")
+    assert_equal version.to_s, output.chomp
+  end
+end
